954fe258f059048df8182600c30c67be46c47c5e
lassebue <lassebuesvendsen@gmail.com>
2015-11-12 14:11:48 +0100

    - Relay add - MyoControl classes added - should be modified further - MainViewModel added with implemented relay

---
+++ CrustCrawlerApp/CrustCrawlerApp/CrustCrawlerApp/WindControl/MainVM.cs
@@ -0,0 +1,31 @@
+﻿using MvvmFoundation.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CrustCrawlerApp.WindControl
+{
+    public class MainVM
+    {
+        // UI commands
+        #region Check Orientation
+        private ICommand _checkOrientationCommand;
+
+        public ICommand CheckOrientationCommandHandler
+        {
+            get { return _checkOrientationCommand ?? (_checkOrientationCommand = new RelayCommand(CheckOrientation)); }
+        }
+
+        private void CheckOrientation()
+        {
+            //var sprintList = ((IEmgSaver)Application.Current.FindResource("SprintListModel"));
+            //MessageBox.Show("Save stuff wtf!!!!");
+        }
+        #endregion
+
+
+    }
+}
