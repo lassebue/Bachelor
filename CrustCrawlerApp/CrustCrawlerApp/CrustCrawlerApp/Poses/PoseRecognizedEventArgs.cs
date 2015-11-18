94c43c0c73fd0a5ab0610edc98c41f47a711361c
lassebue <lassebuesvendsen@gmail.com>
2015-11-19 00:59:46 +0100

    Event args for recognition

---
+++ CrustCrawlerApp/CrustCrawlerApp/CrustCrawlerApp/Poses/PoseRecognizedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrustCrawlerApp.Poses
+{
+
+
+    public delegate void PoseRecognizedHandler(object sender, PoseRecognizedEventArgs e);
+    public class PoseRecognizedEventArgs : EventArgs
+    {
+        public PoseRecognizedEventArgs(Pose pose)
+        {
+            _pose = pose; 
+        }
+        
+        private Pose _pose;
+        public Pose pose
+        {
+            get { return _pose; }
+        }
+
+    }
+}
